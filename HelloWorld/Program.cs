7b1f10dad77c2fecd7ca64cdd127eef8a4538fd2
rajashri03 <telvekar.rajashri@gmail.com>
2022-11-29 08:27:48 +0530

    [Rajashri]-It is hello world program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Namespace-is logical container
+//for grouping purpose we use namepspace
+
+//class-it is blueprint of a specific objects
